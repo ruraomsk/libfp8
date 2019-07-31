37db380dbd599e6965ae23b609400b869383fa46
Rusinov Yury <rusinovyury@gmail.com>
2019-07-31 12:42:43 +0600

    Change float for ir

--- modbus-cpc.c
+++ modbus-cpc.c
@@ -256,8 +256,10 @@ void writeInpRegs(ModbusDevice *md) {
                 break;
             case uint4b:
             case sint4b:
-            case float4b:
                 MODBUS_SET_INT32_TO_INT16(map->tab_input_registers, ir->address, *((int *) ir->value));
+                break;
+            case float4b:
+                modbus_set_float(*(float *)ir->value,(uint16_t *)(map->tab_input_registers+ir->address));
                 break;
             case sint8b:
             case float8b:
@@ -285,8 +287,10 @@ void readInpRegs(ModbusDevice *md) {
                 break;
             case uint4b:
             case sint4b:
-            case float4b:
                 *((int *) ir->value) = MODBUS_GET_INT32_FROM_INT16(map->tab_input_registers, ir->address);
+                break;
+            case float4b:
+                *((float *) ir->value) = modbus_get_float((uint16_t *)(map->tab_input_registers+ ir->address));
                 break;
             case sint8b:
             case float8b:
